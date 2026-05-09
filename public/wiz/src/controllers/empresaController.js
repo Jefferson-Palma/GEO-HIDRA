b86c21ecb2370f39f25cab0593d8aa4a9e56be1b
unknown <vitor.machado@sptech.school>
2026-05-09 14:43:54 -0300

    Adicionando web data wiz com cadastro de funcionario parcial

---
+++ public/wiz/src/controllers/empresaController.js
@@ -0,0 +1,47 @@
+var empresaModel = require("../models/empresaModel");
+
+function buscarPorCnpj(req, res) {
+  var cnpj = req.query.cnpj;
+
+  empresaModel.buscarPorCnpj(cnpj).then((resultado) => {
+    res.status(200).json(resultado);
+  });
+}
+
+function listar(req, res) {
+  empresaModel.listar().then((resultado) => {
+    res.status(200).json(resultado);
+  });
+}
+
+function buscarPorId(req, res) {
+  var idEmpresa = req.params.idEmpresa;
+
+  empresaModel.buscarPorId(id).then((resultado) => {
+    res.status(200).json(resultado);
+  });
+}
+
+function cadastrar(req, res) {
+  var cnpj = req.body.cnpj;
+  var nomeEmpresa = req.body.nomeEmpresa;
+
+  empresaModel.buscarPorCnpj(cnpj).then((resultado) => {
+    if (resultado.length > 0) {
+      res
+        .status(401)
+        .json({ mensagem: `a empresa com o cnpj ${cnpj} já existe` });
+    } else {
+      empresaModel.cadastrar(nomeEmpresa, cnpj).then((resultado) => {
+        res.status(201).json(resultado);
+      });
+    }
+  });
+}
+
+module.exports = {
+  buscarPorCnpj,
+  buscarPorId,
+  cadastrar,
+  listar,
+};
